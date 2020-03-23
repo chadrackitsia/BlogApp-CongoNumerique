ca152610707cf1bce76f1af29ef9cce6815f8336
Chadrack Itsia <itsia001@gmail.com>
2020-03-23 05:21:43 +0100

    Configuration des ROUTES HTTP

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var express = require("express"),
 // CONFIGURATION DE L'APP
 mongoose.connect("mongodb://localhost/congonumerique_db"); // Définition du path et Connection à la BDD
 
-app.set("vieuw engine", "ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }), { useUnifiedTopology: true }, { useNewUrlParser: true }) //  Parse / analyse de l'application
+app.use(bodyParser.urlencoded({ extended: false }, { useUnifiedTopology: true }, { useNewUrlParser: true })) //  Parse / analyse de l'application
 
 // Configuration du Model / Mongoose
 var blogSchema = new mongoose.Schema({
@@ -21,7 +21,15 @@ var blogSchema = new mongoose.Schema({
 var Blog = mongoose.model("Blog", blogSchema); // Définition du nom de Model et du Schema
 
 
+// RESTfull ROUTES
 
+app.get("/", function(req, res){
+  res.redirect("index");
+})
+
+app.get("/blogs", function(req, res){
+  res.render("index");
+})
 
 
 app.listen(8080, function(err){
