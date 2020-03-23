90592387670b1ee87d045d64b5a7f967c625213e
Chadrack Itsia <itsia001@gmail.com>
2020-03-23 03:46:15 +0100

    Configuration DBB et Mongoose Modele Schema

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ var express = require("express"),
   mongoose = require("mongoose"),
   bodyParser = require("body-parser"),
   app = express();
-
-// Connection à la BDD  
-mongoose.connect("mongodb://localhost/congonumerique_db");
+  
+// CONFIGURATION DE L'APP
+mongoose.connect("mongodb://localhost/congonumerique_db"); // Définition du path et Connection à la BDD
 
 app.set("vieuw engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }), { useUnifiedTopology: true }, { useNewUrlParser: true }) //  Parse / analyse de l'application
+
+// Configuration du Model / Mongoose
+var blogSchema = new mongoose.Schema({
+  title : String,
+  image : String,
+  body : String,
+  created : {type : Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema); // Définition du nom de Model et du Schema
+
+
+
+
+
+app.listen(8080, function(err){
+  if(err) {
+    console.log(err)
+  } else {
+    console.log("Server started...")
+  }
+})
 
