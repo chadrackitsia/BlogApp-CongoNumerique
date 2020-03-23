ab0e31cc663255715f37919d1489992d33e3536c
Chadrack Itsia <itsia001@gmail.com>
2020-03-23 18:15:04 +0100

    Connect MongoDB / Mongoose Schema

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var express = require("express"),
   app = express();
   
 // CONFIGURATION DE L'APP
-mongoose.connect("mongodb://localhost/congonumerique_db"); // Définition du path et Connection à la BDD
+mongoose.connect('mongodb://localhost/congonumerique_db', { // Définition du path et Connection à la BDD
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }, { useUnifiedTopology: true }, { useNewUrlParser: true })) //  Parse / analyse de l'application
+app.use(bodyParser.urlencoded({ extended: false })); //  Parse / analyse de l'application
 
 // Configuration du Model / Mongoose
 var blogSchema = new mongoose.Schema({
