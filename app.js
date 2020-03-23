fdc185bfe5ffe59bc53e7833fdc515e3e823abfc
Chadrack Itsia <itsia001@gmail.com>
2020-03-23 03:17:53 +0100

    Configuration et installation des modules

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express = require("express"),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  app = express();
+
+// Connection à la BDD  
+mongoose.connect("mongodb://localhost/congonumerique_db");
+
+app.set("vieuw engine", "ejs");
+
