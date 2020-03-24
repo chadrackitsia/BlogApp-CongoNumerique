fef46e1d0bd96e8f1594f5634172c9ff0e443d4a
Chadrack Itsia <itsia001@gmail.com>
2020-03-24 23:37:16 +0100

    Config Models MongoDB collection

--- models/connexionDB.js
+++ models/connexionDB.js
@@ -1,7 +1,7 @@
 mongoose = require("mongoose");
 
 // *** Définition du path, du nom de la BDD et Connection à la BDD *** //
-connectionDB = function() {
+connectionDB = () => {
   mongoose.connect('mongodb://localhost/congonumerique_db', { 
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -9,7 +9,7 @@ connectionDB = function() {
   if (err){
     console.log(err) // Si la connexion echoue
   } else {
-    console.log("Connection BDD reussie avec succès !!!")
+    console.log("Connection BDD reussie avec succès !!!") // message de succès
   }
 });
 };
