32191cecce7073a9abb351129a76929017a694e7
Chadrack Itsia <itsia001@gmail.com>
2020-03-27 05:59:48 +0100

    Config Athentification routes

--- controllers/routes/users.js
+++ controllers/routes/users.js
@@ -2,5 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-// 
-      
+// Page d'inscription
+router.get('/blogs/inscription', (req, res)=> {
+    res.send('Veuillez remplir le formulaire');
+});
+
+// Connection
+router.get('/blogs/connection', (req, res)=> {
+    res.send('Connection reussie');
+});
