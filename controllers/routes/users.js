9807e2cb56abe46e4b6edc3ad10827449103e1bb
Chadrack Itsia <itsia001@gmail.com>
2020-03-28 13:05:23 +0100

    Définition des structures de controle pour verification formulaires et accès au dashboard

--- controllers/routes/users.js
+++ controllers/routes/users.js
@@ -2,12 +2,52 @@ const express = require('express');
 
 const router = express.Router();
 
+
 // Page d'inscription
-router.get('/blogs/inscription', (req, res)=> {
-    res.send('Veuillez remplir le formulaire');
+router.get('/users/inscription', (req, res)=> {
+  res.render('./users/inscription');
 });
 
-// Connection
-router.get('/blogs/connection', (req, res)=> {
-    res.send('Connection reussie');
+// Page de Connexion
+router.get('/users/connexion',  (req, res) => {
+  res.render('./users/connexion');
 });
+
+// Redirection à la page connection
+router.get('/users', (req, res)=> {
+  res.redirect('./users/connexion');
+});
+
+
+/***  Route inscription des utilisateurs - POST METHOD
+      
+*****/
+router.post('/users/inscription', (req, res) => {
+  const { name, email, password, password2} = req.body;
+  let errors = [];
+
+
+  // Vérification des champs
+  if(!name || !email || !password || password2){
+    errors.push({msg : "Veuillez remplir tous les champs"});
+  }
+
+  // Vérification si les mots de passe saisis sont identiques
+  if(password !== password2){
+    errors.push({msg : "Les mots de passe saisis ne sont pas identiques !"});
+  }
+
+  // Véfication de la longueur de caractès du mot de passe
+  if (password.length < 6) {
+    errors.push({msg : "Le mot de passe doit avoir plus de 6 caractères"});
+  }
+
+  if(errors.length > 0 ) {
+    
+  } else {
+    res.send("Pass !")
+  }
+
+});
+
+module.exports = router;
